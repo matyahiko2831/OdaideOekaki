5ca637affeab8a06583f636b6b710bf72c42f4fc
matyahiko2831 <matyahiko2831@gmail.com>
2016-02-02 00:18:12 +0900

    コピペ

--- app/src/main/java/com/matyahiko2831/odaideoekaki/MainActivity.java
+++ app/src/main/java/com/matyahiko2831/odaideoekaki/MainActivity.java
@@ -8,45 +8,27 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawingView drawingView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        this.drawingView = (DrawingView)findViewById(R.id.drawing_view);
+
+        findViewById(R.id.delete_button).setOnClickListener(deleteDrawing);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    View.OnClickListener deleteDrawing = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            drawingView.delete();
         }
-
-        return super.onOptionsItemSelected(item);
-    }
+    };
 }
