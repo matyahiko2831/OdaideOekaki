c3419689a9892b816bcfcdd34ab3ff5a7afece4e
matyahiko2831 <matyahiko2831@gmail.com>
2016-02-06 19:04:04 +0900

    ナビゲーションビュー追加

---
+++ app/src/main/java/com/matyahiko2831/odaideoekaki/DrawingView.java
@@ -0,0 +1,62 @@
+package com.matyahiko2831.odaideoekaki;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * 画面ペイントクラス
+ * */
+public class DrawingView  extends View {
+
+    private Paint paint;
+    private Path path;
+
+    public DrawingView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        this.path = new Path();
+
+        this.paint = new Paint();
+        paint.setColor(Color.BLACK);
+        this.paint.setStyle(Paint.Style.STROKE);
+        this.paint.setAntiAlias(true);
+        this.paint.setStrokeWidth(10);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawPath(path, paint);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float x = event.getX();
+        float y = event.getY();
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                this.path.moveTo(x, y);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                this.path.lineTo(x, y);
+                break;
+            case MotionEvent.ACTION_UP:
+                this.path.lineTo(x, y);
+                break;
+        }
+        invalidate();
+        return true;
+    }
+
+    public void delete() {
+        this.path.reset();
+        invalidate();
+    }
+}
